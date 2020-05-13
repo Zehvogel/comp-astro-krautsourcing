9e938e4158f39b27646bf14f6487d3508e0a3558
Zehvogel <4reichen@informatik.uni-hamburg.de>
2020-05-13 22:35:25 +0200

    a lot of refactoring

--- exercise2/planets.c
+++ exercise2/planets.c
@@ -7,16 +7,14 @@
 #include <fcntl.h>
 #include <math.h>
 #include <string.h>
-#ifdef USE_RAYLIB
 #include <raylib.h>
-#endif
 
 /*
  * Dimension choices:
  * Length: AU
  * Mass: Earth masses
  * Time: Earth years
- * Velocity: AU / (2*pi earth years) to comply with the given init
+ * Velocity: AU / earth years
  */
 
 #ifdef DOUBLE_PRECISION
@@ -36,9 +34,20 @@ struct bodies {
 	f_t *dy;
 };
 
+enum output_type {
+	RAYLIB,
+	BINFILE
+};
+
+enum integrator {
+	EXPLICIT_EULER,
+	SEMI_IMPLICIT_EULER,
+	VELOCITY_VERLET
+};
+
 typedef signed long loop_t;
 
-void init_planets(struct bodies *planets, loop_t M)
+static void init_planets(struct bodies *planets, loop_t M)
 {
 	planets->M = malloc(M * sizeof(*planets->M));
 	planets->x = malloc(M * sizeof(*planets->x));
@@ -70,7 +79,7 @@ void init_planets(struct bodies *planets, loop_t M)
 	}
 }
 
-void free_planets(struct bodies *planets)
+static void free_planets(struct bodies *planets)
 {
 	free(planets->M);
 	free(planets->x);
@@ -79,44 +88,81 @@ void free_planets(struct bodies *planets)
 	free(planets->dy);
 }
 
-// maybe later
-//static inline f_t velocity_verlet(f_t *x_1, f_t x_2, f_t m_2, f_t *dx_1, f_t dt)
-//{
-//}
-
 int main(int argc, char *argv[])
 {
+	/* needs more refactoring to work with M = 2 again */
 	loop_t M  = 3;
+
 	loop_t t  = 150;
 	float dt = .0026;
+	unsigned FPS = 60;
+	loop_t frameskip = 1;
+	enum integrator integrator = SEMI_IMPLICIT_EULER;
+	enum output_type output = RAYLIB;
+	int file = -1;
 
-	/*TODO: use getopt instead */
-	switch (argc) {
-		case 1:
-			/* nothing to do */
-			break;
-		case 2:
-			if (!sscanf(argv[1], "%lu", &t))
-				return EINVAL;
-			break;
-		case 3:
-			if (!(sscanf(argv[1], "%lu", &t) &&
-					sscanf(argv[2], "%f", &dt)))
-				return EINVAL;
-			break;
-		default:
-			fprintf(stderr, "Illegal parameter count\n");
-			return EINVAL;
+	int opt;
+	while ((opt = getopt(argc, argv, "ht:d:f:s:o:i:")) != -1) {
+		switch (opt) {
+			case 't':
+				if (sscanf(optarg, "%ld", &t))
+					break;
+				else
+					exit(EINVAL);
+			case 'd':
+				if (sscanf(optarg, "%f", &dt))
+					break;
+				else
+					exit(EINVAL);
+			case 'f':
+				if (sscanf(optarg, "%u", &FPS))
+					break;
+				else
+					exit(EINVAL);
+			case 's':
+				if (sscanf(optarg, "%ld", &frameskip))
+					break;
+				else
+					exit(EINVAL);
+			case 'o':
+				if (strcmp(optarg, "raylib") == 0) {
+					output = RAYLIB;
+					break;
+				} else if (strcmp(optarg, "file") == 0) {
+					output = BINFILE;
+					break;
+				} else {
+					exit(EINVAL);
+				}
+			case 'i':
+				if (strcmp(optarg, "EE") == 0) {
+					integrator = EXPLICIT_EULER;
+					break;
+				} else if (strcmp(optarg, "SIE") == 0) {
+					integrator = SEMI_IMPLICIT_EULER;
+					break;
+				} else if (strcmp(optarg, "VV") == 0) {
+					integrator = VELOCITY_VERLET;
+					break;
+				} else {
+					exit(EINVAL);
+				}
+			default: /* fall through */
+			case 'h':
+				printf("Usage: %s [-h] [-t years] [-d stepsize] [-f FPS] [-s frameskip] [-o output: raylib or file] [-i integrator: EE, SIE, VV]\n", argv[0]);
+				exit(EXIT_FAILURE);
+		}
 	}
 
-#ifdef USE_RAYLIB
-	InitWindow(800, 600, "Planets");
-	SetTargetFPS(60);
-#else
-	int file = open("planets.bin", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWRITE);
-	if (file == - 1)
-		return errno;
-#endif
+	if (output == RAYLIB) {
+		InitWindow(800, 600, "Planets");
+		SetTargetFPS(FPS);
+	}
+	if (output == BINFILE) {
+		file = open("planets.bin", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWRITE);
+		if (file == - 1)
+			exit(errno);
+	}
 
 	struct bodies planets;
 
@@ -124,20 +170,16 @@ int main(int argc, char *argv[])
 
 	f_t force_buffer[2 * M];
 	memset(force_buffer, 0, sizeof(force_buffer));
-#ifdef USE_VELOCITY_VERLET
+
+	/* XXX: only needed for VV but M is so small that I don't care */
 	f_t old_force_buffer[2 * M];
 	memset(old_force_buffer, 0, sizeof(force_buffer));
-#endif
 
-#ifdef USE_RAYLIB
-	loop_t frameskip = 10;
 	loop_t i = 0;
-	while (!WindowShouldClose()) {
-#else
-	for (loop_t i = 0; i * dt < t; i++) {
-#endif
+	while (1 == 1) {
 		f_t E = 0;
 		for (loop_t j = 0; j < M; j++) {
+			/* TODO: refactor */
 			for (loop_t k = j + 1; k < M; k++) {
 				f_t r_x = planets.x[j] - planets.x[k];
 				f_t r_y = planets.y[j] - planets.y[k];
@@ -151,109 +193,101 @@ int main(int argc, char *argv[])
 				force_buffer[2*k] -= a * r_x / r * planets.M[j];
 				force_buffer[2*k+1] -= a * r_y / r * planets.M[j];
 
-				// add potential energy
+				/* add potential energy */
 				E += G / r * (planets.M[j] + planets.M[k]);
 			}
-#ifdef USE_VELOCITY_VERLET
-			//given x(t), v(t)
-			//calculate a(t)
-			//
-			//for vv now do
-			//x(t+1) = x(t) + v(t)dt + 0.5a(t)dt^2
-			//calc a(t+1)
-			//v(t+1) = v(t) + 0.5 * (a(t) + a(t+1)) * dt
-
-			//might still be wrong but best working version yet
-
-			//calc initial a
-			if (i != 0) {
-			planets.dx[j] += 0.5 * (force_buffer[2*j]
-						+ old_force_buffer[2*j]) * dt;
-			planets.dy[j] += 0.5 * (force_buffer[2*j+1]
-						+ old_force_buffer[2*j+1]) * dt;
-			planets.x[j] += planets.dx[j] * dt
-					+ 0.5 * force_buffer[2*j] * dt * dt;
-			planets.y[j] += planets.dy[j] * dt
-					+ 0.5 * force_buffer[2*j+1] * dt * dt;
+			/* TODO: refactor */
+			if (integrator == VELOCITY_VERLET) {
+				/* calc initial a */
+				if (i != 0) {
+				planets.dx[j] += 0.5 * (force_buffer[2*j]
+							+ old_force_buffer[2*j]) * dt;
+				planets.dy[j] += 0.5 * (force_buffer[2*j+1]
+							+ old_force_buffer[2*j+1]) * dt;
+				planets.x[j] += planets.dx[j] * dt
+						+ 0.5 * force_buffer[2*j] * dt * dt;
+				planets.y[j] += planets.dy[j] * dt
+						+ 0.5 * force_buffer[2*j+1] * dt * dt;
+				}
+			/* TODO: refactor */
+			} else if (integrator == SEMI_IMPLICIT_EULER) {
+				/*
+				 * explicit euler
+				 * wait this isn't explicit euler
+				 * this is semi-implicit euler
+				 */
+				planets.dx[j] += dt * force_buffer[2*j];
+				planets.dy[j] += dt * force_buffer[2*j+1];
+				planets.x[j] += planets.dx[j] * dt;
+				planets.y[j] += planets.dy[j] * dt;
+			/* TODO: refactor */
+			} else {
+				/* this is explicit euler :( */
+				planets.x[j] += planets.dx[j] * dt;
+				planets.y[j] += planets.dy[j] * dt;
+				planets.dx[j] += dt * force_buffer[2*j];
+				planets.dy[j] += dt * force_buffer[2*j+1];
 			}
-#elif defined(USE_SYMPLECTIC_EULER)
-			// explicit euler
-			// wait this isn't explicit euler
-			// this is semi-implicit euler
-			planets.dx[j] += dt * force_buffer[2*j];
-			planets.dy[j] += dt * force_buffer[2*j+1];
-			planets.x[j] += planets.dx[j] * dt;
-			planets.y[j] += planets.dy[j] * dt;
-#else
-			// this is explicit euler :(
-			planets.x[j] += planets.dx[j] * dt;
-			planets.y[j] += planets.dy[j] * dt;
-			planets.dx[j] += dt * force_buffer[2*j];
-			planets.dy[j] += dt * force_buffer[2*j+1];
-#endif
 
-			// add kinetic energy
+			/* add kinetic energy */
 			E += 0.5 * planets.M[j] * (planets.dx[j] * planets.dx[j]
 						  +planets.dy[j] * planets.dy[j]);
 
-#ifndef USE_RAYLIB
+			/* TODO: refactor */
 			/* write output */
-			if (j == 0)
-				write(file, &E, sizeof(E));
-			write(file, &planets.x[j], sizeof(planets.x[j]));
-			write(file, &planets.y[j], sizeof(planets.y[j]));
+			if (output == BINFILE) {
+				if (j == 0)
+					write(file, &E, sizeof(E));
 
-			f_t L_z = planets.M[j] * (planets.x[j] * planets.dy[j]
-						 -planets.y[j] * planets.dx[j]);
+				write(file, &planets.x[j], sizeof(planets.x[j]));
+				write(file, &planets.y[j], sizeof(planets.y[j]));
 
-			write(file, &L_z, sizeof(L_z));
-#endif
+				f_t L_z = planets.M[j] * (planets.x[j] * planets.dy[j]
+							 -planets.y[j] * planets.dx[j]);
+
+				write(file, &L_z, sizeof(L_z));
+			}
 		}
-#ifdef USE_VELOCITY_VERLET
-		memcpy(old_force_buffer, force_buffer, sizeof(force_buffer));
-#endif
+		if (integrator == VELOCITY_VERLET)
+			memcpy(old_force_buffer, force_buffer, sizeof(force_buffer));
+
 		memset(force_buffer, 0, sizeof(force_buffer));
-#ifdef USE_RAYLIB
-		/* handle with care */
-//		if (IsKeyDown(KEY_UP))
-//			frameskip++;
-//		if (IsKeyDown(KEY_DOWN))
-//			frameskip--;
-//
-		if (i % frameskip == 0) {
 
-		BeginDrawing();
+		if (output == RAYLIB && i % frameskip == 0) {
 
-		ClearBackground(BLACK);
+			BeginDrawing();
 
-		DrawText(TextFormat("frameskip: %d", frameskip), 10, 30, 20, DARKGREEN);
+			ClearBackground(BLACK);
 
-		DrawFPS(10,10);
+			DrawText(TextFormat("frameskip: %d", frameskip), 10, 30, 20, DARKGREEN);
 
-		Vector2 sunPos = {50*planets.x[0]+400, 50*planets.y[0]+300};
-		DrawCircleV(sunPos, 20, GOLD);
+			DrawFPS(10,10);
 
-		Vector2 earthPos = {50*planets.x[1]+400, 50*planets.y[1]+300};
-		DrawCircleV(earthPos, 5, BLUE);
+			Vector2 sunPos = {50*planets.x[0]+400, 50*planets.y[0]+300};
+			DrawCircleV(sunPos, 20, GOLD);
 
-		Vector2 jupPos = {50*planets.x[2]+400, 50*planets.y[2]+300};
-		DrawCircleV(jupPos, 9, BEIGE);
+			Vector2 earthPos = {50*planets.x[1]+400, 50*planets.y[1]+300};
+			DrawCircleV(earthPos, 5, BLUE);
 
-		EndDrawing();
+			Vector2 jupPos = {50*planets.x[2]+400, 50*planets.y[2]+300};
+			DrawCircleV(jupPos, 9, BEIGE);
+
+			EndDrawing();
 
 		}
-
 		i++;
-#endif
+		if (output == RAYLIB && WindowShouldClose())
+			break;
+		if (i * dt >= t)
+			break;
 	}
 
 	free_planets(&planets);
-#ifdef USE_RAYLIB
-	CloseWindow();
-#else
-	close(file);
-#endif
 
+	if (output == RAYLIB)
+		CloseWindow();
+	if (output == BINFILE)
+		close(file);
 
 	return EXIT_SUCCESS;
 }
